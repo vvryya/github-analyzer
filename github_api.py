0f6492029ee5fec075dd77b86cf1f2697d4fa808
Varya Pavlova <vvryya@Varyas-MacBook-Air.local>
2025-07-30 23:35:40 +1000

    add get activity

--- github_api.py
+++ github_api.py
@@ -19,3 +19,21 @@ def get_user_repos(username, token = None):
         })
 
     return results
+
+def get_commit_activity(username, token = None):
+    headers = {"Authorization": f"token {token}"} if token else {}
+    url = f"https://api.github.com/users/{username}/repos"
+    repos_resp = requests.get(url, headers = headers)
+
+    data = []
+    for repo in repos_resp.json():
+        repo_url = f"https://api.github.com/users/{username}/{repo["name"]}/stats/commit_activity"
+        r = requests.get(repo_url, headers = headers)
+        if r.status_code == 202:
+            print("GitHub is calculating stats, need to try later")
+            continue
+        if r.ok:
+            weeks = r.json()
+            total = sum(week["total"] for week in weeks)
+            data.append({"repo": repo["name"], "commits": total})
+    return data
